72459112d6b4544dc868259a7d01641ebb9c1237
Valentina Endovitskaya <valyalark@hotmail.com>
2018-10-28 16:40:24 +0300

    fixed ambigous slug bug

--- product.php
+++ product.php
@@ -6,7 +6,7 @@
 
 	try{
 		 		
-	    $stmt = $conn->prepare("SELECT *, products.name AS prodname, category.name AS catname, products.id AS prodid FROM products LEFT JOIN category ON category.id=products.category_id WHERE slug = :slug");
+	    $stmt = $conn->prepare("SELECT *, products.name AS prodname, category.name AS catname, products.id AS prodid FROM products LEFT JOIN category ON category.id=products.category_id WHERE products.slug = :slug");
 	    $stmt->execute(['slug' => $slug]);
 	    $product = $stmt->fetch();
 		
