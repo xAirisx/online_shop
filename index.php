26183689d6519dfe21a602be7e932fba6618f9fc
Valentina Endovitskaya <valyalark@hotmail.com>
2018-10-28 16:52:45 +0300

    removed 3rd slide

--- index.php
+++ index.php
@@ -26,7 +26,6 @@
 		                <ol class="carousel-indicators">
 		                  <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
 		                  <li data-target="#carousel-example-generic" data-slide-to="1" class=""></li>
-		                  <li data-target="#carousel-example-generic" data-slide-to="2" class=""></li>
 		                </ol>
 		                <div class="carousel-inner">
 		                  <div class="item active">
@@ -34,9 +33,6 @@
 		                  </div>
 		                  <div class="item">
 		                    <img src="images/banner2.png" alt="Second slide">
-		                  </div>
-		                  <div class="item">
-		                    <img src="images/banner3.png" alt="Third slide">
 		                  </div>
 		                </div>
 		                <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
